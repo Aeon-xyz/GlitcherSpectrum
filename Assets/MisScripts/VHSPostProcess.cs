31796fdb74c650391082bbe8fbd1bf20e4cdf7ef
Zero-Ax <ax.161614@gmail.com>
2016-06-05 23:37:16 -0500

    First Commit
    First

---
+++ Assets/MisScripts/VHSPostProcess.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Camera))]
+public class VHSPostProcess : MonoBehaviour {
+    public static Material m_Material = null;
+    protected Material material
+    {
+        get
+        {
+            if (m_Material == null)
+            {
+                m_Material = new Material(shader);
+                m_Material.SetTexture("_VHSTex", VHS);
+                m_Material.hideFlags = HideFlags.DontSave;
+            }
+            return m_Material;
+        }
+    }
+
+    public Shader shader;
+    public MovieTexture VHS;
+
+    float yScanline, xScanline;
+
+    void Start()
+    {
+        //m = new Material(shader);
+        //m.SetTexture("_VHSTex", VHS);
+        //m.hideFlags = HideFlags.DontSave;
+        VHS.loop = true;
+        VHS.Play();
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        yScanline += Time.deltaTime * 0.01f;
+        xScanline -= Time.deltaTime * 0.1f;
+
+        if (yScanline >= 1)
+        {
+            yScanline = Random.value;
+        }
+        if (xScanline <= 0 || Random.value < 0.05)
+        {
+            xScanline = Random.value;
+        }
+        material.SetFloat("_yScanline", yScanline);
+        material.SetFloat("_xScanline", xScanline);
+        Graphics.Blit(source, destination, material);
+    }
+
+}
